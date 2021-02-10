f8a874c2194ac3f8345ee90088637013956c7f47
Young wook Jeon <ela87@naver.com>
2021-02-10 19:44:52 +0900

    Refactoring and Separating our Envs

---
+++ src/user/UserService.js
@@ -0,0 +1,10 @@
+const User = require('./User');
+const bcypt = require('bcrypt');
+
+const save = async (body) => {
+  const hash = await bcypt.hash(body.password, 10);
+  const user = { ...body, password: hash };
+  await User.create(user);
+};
+
+module.exports = { save };
