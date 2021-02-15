1f8595c3212057fd310394c510911f0b7f24c841
Young wook Jeon <ela87@naver.com>
2021-02-15 18:46:45 +0900

    Verify whether or not E-mail is in use

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -11,7 +11,19 @@ router.post(
     .bail()
     .isLength({ min: 4, max: 32 })
     .withMessage('Must have min 4 and max 32 characters'),
-  check('email').notEmpty().withMessage('E-mail cannot be null').bail().isEmail().withMessage('E-mail is not valid'),
+  check('email')
+    .notEmpty()
+    .withMessage('E-mail cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('E-mail is not valid')
+    .bail()
+    .custom(async (email) => {
+      const user = await UserService.findByEmail(email);
+      if (user) {
+        throw new Error('E-mail in use');
+      }
+    }),
   check('password')
     .notEmpty()
     .withMessage('Password cannot be null')
@@ -29,7 +41,6 @@ router.post(
       return res.status(400).send({ validationErrors: validationErrors });
     }
     await UserService.save(req.body);
-
     return res.send({ message: 'User created' });
   }
 );
